cb7f9541e2d0f8d05873939063ae031f5473722c
Jesus Hernandez <jesushdzusa@gmail.com>
2026-02-26 14:19:05 -0600

    refactoring upload endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const { resetPassword, login, authStatus, logout, setup2FA, verify2FA, reset2FA, verify2FASetup } = require('../controllers/authController');
 const router = express.Router();
 const dotenv = require('dotenv').config();
+const formData = require('form-data');
 
 const upload = multer(); // Configuración de multer para recibir archivos multiform-data
 console.log("dotenv:", dotenv.parsed);
@@ -75,7 +76,7 @@ router.post('/upload', upload.array('file', 10), async (req, res) => {
     //Forward files to microservice
     const formData = new FormData();
     for (const file of req.files) {
-      formData.append('file', file.buffer, file.originalname);
+      formData.append('file', file.buffer, {filename: file.originalname, contentType: file.mimetype});
     }
     const response = await fetch(`${process.env.URL_MICROSERVICE}/upload`, {
       method: 'POST',
